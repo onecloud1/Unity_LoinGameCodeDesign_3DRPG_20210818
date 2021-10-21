9b665742cd1f3e68c28e2a0274404775ff70f726
onecloud1 <d1010234@gmail.com>
2021-10-21 12:00:27 +0800

    2021.10.21 中午備份
    2021.10.21 中午備份

---
+++ Unity_LoinGameCodeDesign_3DRPG_20210818/Assets/Scripts/DataDialogue.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace WEI.Dialogue
+{
+    /// <summary>
+    /// 對話系統的資料
+    /// NPC要對話的三階段
+    /// 接任務前.進行中.完成任務
+    /// </summary>
+    /// ScriptableObject 繼承此類別會變腳本化物件,可將腳本資料當物件保存在專案
+    /// CreateAssetMenu 類別屬性:為此類別建立專內選單
+    [CreateAssetMenu(menuName = "WEI/對話資料",fileName = "NPC對話資料")]
+    public class DataDialogue : ScriptableObject
+    {
+        [Header("任務前對話內容"),TextArea(2,7)]
+        public string[] beforeMission;
+        [Header("任務進行中對話內容"), TextArea(2, 7)]
+        public string[] missionning;
+        [Header("任務完成對話內容"), TextArea(2, 7)]
+        public string[] afterMission;
+        [Header("任務需求數量"),Range(0,100) ]
+        public int countNeed;
+        [Header("NPC 任務狀態")]
+        public StateNPCMission StateNPCMission = StateNPCMission.BeforeMission;
+    }
+    //陣列: 保存相同資料類型的結構
+}
---
+++ Unity_LoinGameCodeDesign_3DRPG_20210818/Assets/Scripts/StateNPCMission.cs
@@ -0,0 +1,9 @@
+namespace WEI.Dialogue
+{
+    //列舉:下拉式選單,可自行定義選項
+    //語法:修飾詞 列舉 列舉名稱 {1,2,3,...}
+    public enum StateNPCMission
+    {
+        BeforeMission, Missionning, AfterMission
+    }
+}
